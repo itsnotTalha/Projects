b7f43a2d10234d18f5ae1554e8c53d7b929f765a
itsnotTalha <talhajubayer737@gmail.com>
2024-12-07 07:02:41 +0600

    Added password verification

--- UCAM/src/UIU.java
+++ UCAM/src/UIU.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class UIU {
+    Scanner scan = new Scanner(System.in);
     public String StudentName;
     public String StudentID;
     private String StudentPass;
     private double cgpa;
     public int courses;
     public int credit;
+    public int total_credit;
 
     private double discount;
 
@@ -15,15 +19,38 @@ public class UIU {
     public UIU(){
 
     }
+    public UIU(String ugID, String ugPass){
+        System.out.print("Enter UG name: ");
+        this.UGName=scan.nextLine();
+        this.UGID=ugID;
+        this.UGPass=ugPass;
+    }
+
     public UIU(String studentName, String studentID, String studentPass){
         this.StudentName=studentName;
         this.StudentID=studentID;
         this.StudentPass=studentPass;
     }
 
+    public boolean checkPass() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your password: ");
+        String pass = scan.next();
+        if (pass.equals(StudentPass)) {
+            return true;
+        } else if (pass.equals(UGPass)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public void Display(){
+        if(checkPass()){
         System.out.println("Name: "+StudentName);
         System.out.println("ID: "+StudentID);
+            System.out.println("Completed Credit: "+total_credit);
+            System.out.println("CGPA: "+cgpa);
+        }
     }
 
     public String getStudentPass() {
